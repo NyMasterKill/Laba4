d060a773471079a153dffc24f11474873afb58cb
Kirill <190339274+NyMasterKill@users.noreply.github.com>
2025-12-06 17:27:31 +0500

    Сгенерировать User, Profile, GosuslugiBinding модели

---
+++ src/types/userModels.ts
@@ -0,0 +1,6 @@
+// Файл для демонстрации сгенерированных моделей ORM (задача 1.3.1)
+// Эти модели уже были созданы в рамках задач 1.2.1
+
+export { User } from '../entities/User';
+export { Profile } from '../entities/Profile';
+export { GosuslugiBinding } from '../entities/GosuslugiBinding';
