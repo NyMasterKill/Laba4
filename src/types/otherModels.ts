b9b1d90ff673dca80c09b68983ff4c125c820711
Kirill <190339274+NyMasterKill@users.noreply.github.com>
2025-12-06 17:29:00 +0500

    Сгенерировать Station, Booking, Ride, Payment, Fine

---
+++ src/types/otherModels.ts
@@ -0,0 +1,8 @@
+// Файл для демонстрации сгенерированных моделей ORM (задача 1.3.3)
+// Эти модели уже были созданы в рамках задач 1.2.2
+
+export { Station } from '../entities/Station';
+export { Booking } from '../entities/Booking';
+export { Ride } from '../entities/Ride';
+export { Payment } from '../entities/Payment';
+export { Fine } from '../entities/Fine';
