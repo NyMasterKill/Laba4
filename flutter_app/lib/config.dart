1db5faaf0ad414fcc4ad23f0a790d44eab884d89
Kirill <190339274+NyMasterKill@users.noreply.github.com>
2025-12-07 11:22:09 +0500

    Создать файл конфигурации и добавить заглушку для Yandex MapKit API ключа

---
+++ flutter_app/lib/config.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  // API Key for Yandex MapKit
+  // This should be replaced with the actual API key obtained from Yandex Developer Portal
+  static const String yandexMapKitApiKey = 'YOUR_YANDEX_MAPKIT_API_KEY_HERE';
+}
